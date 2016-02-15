2788a1d95ef1404027a25f9abb4ddbfcc3d6a216
Zack Winchell <zackwinchell@gmail.com>
2016-02-15 10:51:02 -0800

    sorting almost implemented

--- public/js/main.js
+++ public/js/main.js
@@ -3,7 +3,8 @@
 var app = angular.module("toDoList", []);
 
 app.controller("toDoCtrl", function($http, $scope){
-  console.log("toDoCtrl");
+
+  $scope.sort = "+name";
 
   $http.get("/tasks")
     .then(function(res){
@@ -12,26 +13,43 @@ app.controller("toDoCtrl", function($http, $scope){
       console.error(err);
     });
 
-    $scope.addTask = function() {
-      $http.post("/tasks", $scope.task)
-        .then(function(res){
-          $scope.tasks.push(res.data);
-        }, function(err){
-          console.error(err);
-        })
-    }
+  $scope.addTask = function() {
+    $http.post("/tasks", $scope.task)
+    .then(function(res){
+      $scope.tasks.push(res.data);
+    }, function(err){
+      console.error(err);
+    })
+  }
 
-    $scope.deleteTask = function() {
-      var thisTask = this;
-      $http.delete(`/tasks/${this.task._id}`)
-        .then(function(res) {
-          $scope.tasks.splice(thisTask.$index, 1);
-        },function(err) {
-          return console.error(err);
-        });
-    }
+  $scope.deleteTask = function() {
+    var thisTask = this;
+    $http.delete(`/tasks/${this.task._id}`)
+      .then(function(res) {
+        $scope.tasks.splice(thisTask.$index, 1);
+      },function(err) {
+        return console.error(err);
+      });
+  }
 
-    $scope.toggleComplete = function() {
-      console.log(this);
+  $scope.sortBy = function(clicked) {
+    console.log("$scope.sort:", $scope.sort);
+    console.log(clicked);
+    var sort = $scope.sort;
+    if(sort === clicked){
+      if(sort.charAt(0) === "+"){
+        $scope.sort = `-${sort}`;
+      } else {
+        $scope.sort = `+${sort}`;
+      }
+    } else {
+      $scope.sort = `+${clicked}`;
     }
+  }
+
+  $scope.toggleComplete = function($event) {
+    // $http.put(`/tasks/${this.}`)
+    console.log($event.currentTarget);
+    // console.log(this);
+  }
 });
